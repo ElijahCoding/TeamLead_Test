6f405cccc23936b4cf3f93142928f7bcc1c2f616
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-13 12:20:16 +0300

    category parent-children

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     protected $guarded = [];
     
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'category_product');
+    }
 }
--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -2,15 +2,15 @@
 
 namespace Tests\Unit\Models;
 
-use App\Models\Category;
+use App\Models\{Category, Product};
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
     public function test_a_category_belongs_to_many_products()
     {
-        $category = factory('App\Models\Category')->create();
-        $product = factory('App\Models\Product')->create();
+        $category = factory(Category::class)->create();
+        $product = factory(Product::class)->create();
 
         $category->prodcuts()->attach($product);
 
@@ -18,5 +18,21 @@ class CategoryTest extends TestCase
             'category_id' => $category->id,
             'product_id' => $product->id
         ]);
+    }
+
+    public function test_a_category_has_a_parent()
+    {
+
+    }
+
+    public function test_a_category_has_many_children()
+    {
+        $category = factory(Category::class)->create();
+
+        $category->children()->save(
+            factory(Category::class)->create()
+        );
+        
+        $this->assertInstanceOf(Category::class, $category->children()->first());
     }
 }
