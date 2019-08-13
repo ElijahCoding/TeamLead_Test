5d2c1a0d96c18ca528d296bb6d94281a285f66ca
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-13 12:17:09 +0300

    category test

--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -2,12 +2,21 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Category;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
-    public function testExample()
+    public function test_a_category_belongs_to_many_products()
     {
-        $this->assertTrue(true);
+        $category = factory('App\Models\Category')->create();
+        $product = factory('App\Models\Product')->create();
+
+        $category->prodcuts()->attach($product);
+
+        $this->assertDatabaseHas('category_product', [
+            'category_id' => $category->id,
+            'product_id' => $product->id
+        ]);
     }
 }
